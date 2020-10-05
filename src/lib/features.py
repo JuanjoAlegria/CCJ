62d01b8071ebc418538491764a62693297206e6c
Juan José Alegría <jotaj.8a@gmail.com>
2020-10-05 05:53:53 -0300

    Changes in image depth for area and skeleton features

--- src/lib/features.py
+++ src/lib/features.py
@@ -10,10 +10,11 @@ from skimage.filters.rank import entropy
 from skimage.morphology import medial_axis
 from skan import skeleton_to_csgraph, _testdata, draw, Skeleton, summarize
 from . import centroids as centutils
+from . import utils
 
-def mask_results(results, jbin_img): 
+def mask_results(results, jbin_img):
     results_copy = results.copy()
-    results_copy[jbin_img == 0] = 0    
+    results_copy[jbin_img == 0] = 0
     return results_copy
 
 def window_stdev(img, window_size):
@@ -35,7 +36,7 @@ def voronoi_approx_statistics(img, raw_values=False):
     distance_skel[~mask] = 0
     non_zero = distance_skel[mask]
     if raw_values:
-        return non_zero.mean(), non_zero.std(), distance_skel 
+        return non_zero.mean(), non_zero.std(), distance_skel
     return non_zero.mean(), non_zero.std()
 
 
@@ -45,15 +46,15 @@ def medial_axis_stadistics(img, raw_values=False):
     dist_on_skel = distance * skel
     non_zero = dist_on_skel[np.where(dist_on_skel != 0)]
     if raw_values:
-        return non_zero.mean(), non_zero.std(), dist_on_skel 
+        return non_zero.mean(), non_zero.std(), dist_on_skel
     return non_zero.mean(), non_zero.std()
 
 def std_filter_statistics(img, jbin_img, raw_values=False):
-    filtered = window_stdev(img, window_size=7)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+    filtered = window_stdev(img, window_size=7)
     mask_filtered = mask_results(filtered, jbin_img)
     non_zero = mask_filtered[np.where(mask_filtered != 0)]
     if raw_values:
-        return non_zero.mean(), non_zero.std(), mask_filtered 
+        return non_zero.mean(), non_zero.std(), mask_filtered
     return non_zero.mean(), non_zero.std()
 
 def entropy_filter_statistics(img, jbin_img, raw_values=False):
@@ -61,13 +62,13 @@ def entropy_filter_statistics(img, jbin_img, raw_values=False):
     mask_filtered = mask_results(filtered, jbin_img)
     non_zero = mask_filtered[np.where(mask_filtered != 0)]
     if raw_values:
-        return non_zero.mean(), non_zero.std(), mask_filtered 
+        return non_zero.mean(), non_zero.std(), mask_filtered
     return non_zero.mean(), non_zero.std()
 
 def get_skeleton_data(sk_img):
-    sk_img_gray = cv2.cvtColor(sk_img, cv2.COLOR_BGR2GRAY)
-    pixel_graph, coordinates, degrees = skeleton_to_csgraph(sk_img_gray)
-    branch_data = summarize(Skeleton(sk_img_gray))
+    # sk_img_gray = cv2.cvtColor(sk_img, cv2.COLOR_BGR2GRAY)
+    pixel_graph, coordinates, degrees = skeleton_to_csgraph(sk_img)
+    branch_data = summarize(Skeleton(sk_img))
     return degrees, branch_data
 
 
@@ -75,28 +76,27 @@ def get_area_features(nuclei_img, jbin_img):
     centroids = centutils.get_nuclei_centroids(nuclei_img)
     centroids = centutils.clean_centroids(centroids, jbin_img)
     centroids, moments = centutils.get_moments_cells(centroids, jbin_img)
-    
+
     ret = {}
-    
+
     cell_area = 0
     total_area = jbin_img.shape[0] * jbin_img.shape[1]
-    
+
     for m in moments:
         cell_area += m['m00']
-    
-    # white_area = len(jbin_img[np.where(jbin_img != 0)]) 
-    white_area = len(jbin_img[np.all(jbin_img == [255,255,255], axis=-1)]) 
+
+    white_area = len(jbin_img[jbin_img == 255])
     ret['cell_area_ratio'] = cell_area / total_area
     ret['white_area_ratio'] = white_area / total_area
-    
+
     return ret
 
 
 def get_skeleton_features(sk_img, raw_data=''):
     degrees, branch_data = get_skeleton_data(sk_img)
-    
+
     ret = {}
-    
+
     # End-to-end
     e2e_data = branch_data[branch_data['branch-type'] == 0]
     ret['e2e_n'] = len(e2e_data)
@@ -106,7 +106,7 @@ def get_skeleton_features(sk_img, raw_data=''):
     ret['e2e_eu_distance_std'] = e2e_data['euclidean-distance'].std()
     ret['e2e_distance_ratio_mean'] = (e2e_data['euclidean-distance'] / e2e_data['branch-distance']).mean()
     ret['e2e_distance_ratio_std'] = (e2e_data['euclidean-distance'] / e2e_data['branch-distance']).std()
-    
+
     # Junction-to-end
     j2e_data = branch_data[branch_data['branch-type'] == 1]
     ret['j2e_n'] = len(j2e_data)
@@ -116,7 +116,7 @@ def get_skeleton_features(sk_img, raw_data=''):
     ret['j2e_eu_distance_std'] = j2e_data['euclidean-distance'].std()
     ret['j2e_distance_ratio_mean'] = (j2e_data['euclidean-distance'] / j2e_data['branch-distance']).mean()
     ret['j2e_distance_ratio_std'] = (j2e_data['euclidean-distance'] / j2e_data['branch-distance']).std()
-    
+
     # Junction-to-junction
     j2j_data = branch_data[branch_data['branch-type'] == 2]
     ret['j2j_n'] = len(j2j_data)
@@ -133,7 +133,7 @@ def get_skeleton_features(sk_img, raw_data=''):
     ret['nodes_max'] = nodes.max()
     ret['nodes_mean'] = nodes.mean()
     ret['nodes_std'] = nodes.std()
-    
+
     return ret
 
 def get_branch_thickness_features(jbin_img):
@@ -149,7 +149,7 @@ def get_branch_thickness_features(jbin_img):
     return ret
 
 def get_texture_features(original_img, jbin_img):
-    ret = {} 
+    ret = {}
     original_img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     jbin_img = cv2.cvtColor(jbin_img, cv2.COLOR_BGR2GRAY)
     sf_mean, sf_std = std_filter_statistics(original_img, jbin_img)
@@ -163,15 +163,37 @@ def get_texture_features(original_img, jbin_img):
     return ret
 
 def get_features(original_img, nuclei_img, jbin_img, jsk_img):
-    features = {}
-    area_ft = get_area_features(nuclei_img, jbin_img)
-    sk_ft = get_skeleton_features(jsk_img)
-    bt_ft = get_branch_thickness_features(jbin_img)
-    tx_ft = get_texture_features(original_img, jbin_img)
+    """[summary]
 
-    features.update(area_ft)
+    Args:
+        original_img (np.ndarray, shape=(n,n), dtype=np.uint16): Cell-cell
+            junction image, one channel.
+        nuclei_img (np.ndarray, shape=(n,n), dtype=np.uint16): Nuclei image, one
+            channel.
+        jbin_img (np.ndarray, shape=(n,n), dtype=np.uint8): Segmented cell-cell
+            junction image, binary image.
+        jsk_img (np.ndarray, shape=(n,n), dtype=np.uint8): Skeletonized
+            cell-cell junction image, binary image.
+
+    Returns:
+        dict[str->float]: computed features over the image.
+    """
+    features = {}
+
+    # To compute the area functions, we use a opencv functions, and most of
+    # those require 8-bit, 3 channels images.
+    # nuclei_img_bgr = utils.convert_16_gray_to_8_bgr(nuclei_img)
+    # jbin_img_bgr = cv2.cvtColor(jbin_img, cv2.COLOR_GRAY2BGR)
+
+    #area_ft = get_area_features(nuclei_img_bgr, jbin_img)
+
+    sk_ft = get_skeleton_features(jsk_img)
+    # bt_ft = get_branch_thickness_features(jbin_img)
+    # tx_ft = get_texture_features(original_img, jbin_img)
+
+    #features.update(area_ft)
     features.update(sk_ft)
-    features.update(bt_ft)
-    features.update(tx_ft)
+    # features.update(bt_ft)
+    # features.update(tx_ft)
 
     return features
