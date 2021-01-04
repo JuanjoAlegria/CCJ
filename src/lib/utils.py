c81fdfeaa80d03fd8a5694df0cf826305b4e1a1c
Juan José Alegría <jotaj.8a@gmail.com>
2021-01-04 09:44:32 -0300

    Documentation of load_images

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -2,34 +2,46 @@ import os
 import cv2
 import numpy as np
 
+
 def crop_image(img, new_size):
     height, width = img.shape[:2]
     new_height, new_width = new_size
     i_coord = (height - new_height) // 2
     j_coord = (width - new_width) // 2
-    return img[i_coord: i_coord + new_height, j_coord: j_coord + new_width]
+    return img[i_coord : i_coord + new_height, j_coord : j_coord + new_width]
+
 
 def convert_16_gray_to_8_bgr(img16):
-    img8 = (img16 / 256).astype('uint8')
+    img8 = (img16 / 256).astype("uint8")
     img8_bgr = cv2.cvtColor(img8, cv2.COLOR_GRAY2BGR)
     return img8_bgr
 
+
 def convert_16_to_8(img16):
-    img8 = (img16 / 256).astype('uint8')
+    img8 = (img16 / 256).astype("uint8")
     return img8
 
 
 def load_images(imgs_dir, img_name):
-    nuclei_path = os.path.join(imgs_dir, f'Nuclei/{img_name}n.tif')
-    ccj_path = os.path.join(imgs_dir, f'CCJ/{img_name}j.tif')
-    seg_path = os.path.join(imgs_dir, f'Segmented-CCJ/{img_name}j-BI.tif')
-    sk_path = os.path.join(imgs_dir, f'Skeletonized-CCJ/{img_name}j-SK.tif')
+    """Load all the versions of the same image.
+
+    Args:
+        imgs_dir (str): Base folder of the images.
+        img_name (str): Name of the image.
+
+    Returns:
+        (np.ndarray(n,n), np.ndarray(n,n), np.ndarray(n,n), np.ndarray(n,n)):
+            nuclei_img, ccj_img, seg_img, sk_img
+    """
+    nuclei_path = os.path.join(imgs_dir, f"Nuclei/{img_name}n.tif")
+    ccj_path = os.path.join(imgs_dir, f"CCJ/{img_name}j.tif")
+    seg_path = os.path.join(imgs_dir, f"Segmented-CCJ/{img_name}j-BI.tif")
+    sk_path = os.path.join(imgs_dir, f"Skeletonized-CCJ/{img_name}j-SK.tif")
 
     nuclei_img = cv2.imread(nuclei_path, cv2.IMREAD_UNCHANGED)
     ccj_img = cv2.imread(ccj_path, cv2.IMREAD_UNCHANGED)
     seg_img = cv2.imread(seg_path, cv2.IMREAD_UNCHANGED)
     sk_img = cv2.imread(sk_path, cv2.IMREAD_UNCHANGED)
-
 
     assert nuclei_img.shape == ccj_img.shape
 
@@ -42,11 +54,11 @@ def load_images(imgs_dir, img_name):
     return nuclei_img, ccj_img, seg_img, sk_img
 
 
-
 def resize_up_to(img, min_size):
     scale_factor = int(np.ceil(min_size / min(img.shape)))
     new_size = scale_factor * np.array(img.shape)
     return cv2.resize(img, (new_size[1], new_size[0]))
+
 
 def contour_to_image(cnt):
     """Generates a binary image from a contour.
@@ -60,6 +72,6 @@ def contour_to_image(cnt):
     """
     bbox = cv2.boundingRect(cnt)
     norm_cnt = cnt - bbox[:2]
-    img = np.zeros((bbox[3], bbox[2]), dtype='uint8')
+    img = np.zeros((bbox[3], bbox[2]), dtype="uint8")
     cv2.fillPoly(img, pts=[norm_cnt], color=255)
     return img
